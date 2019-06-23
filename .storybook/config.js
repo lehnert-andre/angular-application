6f0259dde316bc0f19cfc2cb6e8e310e66cb52fb
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2019-06-23 22:44:33 +0200

    Switch to Typescript Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
 import { configure } from '@storybook/angular';
 
+// automatically import all files ending in *.stories.ts
+const req = require.context('../stories', true, /\.stories\.ts$/);
 function loadStories() {
-    require('../stories/index.js');
-    // You can require as many stories as you need.
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
